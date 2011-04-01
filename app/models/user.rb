9f508ec5aeb9c9db8aa0e9ff8e370858a5c92c88
Guilherme Ceolin <guiceolin@gmail.com>
2011-04-01 09:11:30 -0300

    Fix for new fields on registration, using devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :first_name, :last_name
 
   # Validations.
   validates_presence_of :first_name
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,8 @@ describe User do
   it { should validate_presence_of :email }
   it { should validate_presence_of :first_name }
   it { should validate_presence_of :last_name }
+  it { should allow_mass_assignment_of :first_name }
+  it { should allow_mass_assignment_of :last_name }
 
   context '#full_name' do
     let(:user) { Factory :user , :first_name => 'John', :last_name => 'Locke' }
