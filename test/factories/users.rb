086ac8bd22a57dcb719019920b16cf023a27c92c
Guilherme Ceolin <guiceolin@gmail.com>
2011-05-14 10:18:50 -0300

    Updating Factory for users

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -9,3 +9,32 @@ Factory.define :user do |f|
   f.last_name 'Last Name'
 
 end
+
+Factory.define :admin do |f|
+  #f.sequence(:first_name) { |n| "User created by a factory##{n}" }
+  f.sequence(:email) { |n| "user#{n}@example.org" }
+  f.password 'q1w2e3r4'
+  f.password_confirmation 'q1w2e3r4'
+  f.first_name 'First name'
+  f.last_name 'Last Name'
+end
+
+Factory.define :manager do |f|
+  #f.sequence(:first_name) { |n| "User created by a factory##{n}" }
+  f.sequence(:email) { |n| "user#{n}@example.org" }
+  f.password 'q1w2e3r4'
+  f.password_confirmation 'q1w2e3r4'
+  f.first_name 'First name'
+  f.last_name 'Last Name'
+
+end
+
+Factory.define :member do |f|
+  #f.sequence(:first_name) { |n| "User created by a factory##{n}" }
+  f.sequence(:email) { |n| "user#{n}@example.org" }
+  f.password 'q1w2e3r4'
+  f.password_confirmation 'q1w2e3r4'
+  f.first_name 'First name'
+  f.last_name 'Last Name'
+
+end
