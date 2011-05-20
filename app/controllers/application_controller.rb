871c86a323376d54c051a9c7fcc2acacdca3cb2d
Guilherme Ceolin <guiceolin@gmail.com>
2011-05-20 01:48:42 -0300

    Adding CanCan rescue to AppController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, :alert => exception.message
+  end
   protect_from_forgery
 end
