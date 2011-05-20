70d4432b4b4d637a4c892ab863c5319fe5d5df66
Guilherme Ceolin <guiceolin@gmail.com>
2011-05-20 01:23:33 -0300

    Fixing Devise STI

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,8 @@
 class User < ActiveRecord::Base
 
   has_many :memberships
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable, :lockable and :timeoutable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
+ 
+  devise :database_authenticatable, :recoverable
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   attr_accessible :first_name, :last_name
