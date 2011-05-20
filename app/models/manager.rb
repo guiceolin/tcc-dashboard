3aa5cb7f9d0507629ecaaaf6ecaa08a27aa70a96
Guilherme Ceolin <guiceolin@gmail.com>
2011-05-20 01:23:53 -0300

    Method in manager to associate a project

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -3,4 +3,8 @@ class Manager < Member
   has_many :managed_projects, :through    => :memberships, 
                               :source     => :project, 
                               :conditions => 'memberships.manager = true'
+
+  def associate_managed_project(project)
+    Membership.create(:project => project, :user => self, :manager => true)
+  end
 end
