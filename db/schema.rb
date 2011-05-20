8b34cd6a65f28f5b77b7ec8c0a37a8655e7790dc
Guilherme Ceolin <guiceolin@gmail.com>
2011-05-20 01:31:29 -0300

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110327142057) do
+ActiveRecord::Schema.define(:version => 20110513223914) do
+
+  create_table "memberships", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "project_id"
+    t.boolean  "manager",    :default => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "projects", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                               :default => "", :null => false
@@ -29,6 +43,7 @@ ActiveRecord::Schema.define(:version => 20110327142057) do
     t.string   "first_name"
     t.string   "last_name"
     t.integer  "roles_mask"
+    t.string   "type",                                                :null => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
