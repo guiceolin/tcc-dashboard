d427300022f6329c85c33b91dae6f071ac65d024
Guilherme Ceolin <guiceolin@gmail.com>
2011-05-20 01:29:46 -0300

    true == 1; false ==0

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,12 +1,13 @@
 class Project < ActiveRecord::Base
+
   has_many :memberships
   has_many :members,  :through    => :memberships, 
                       :source     => :user, 
-                      :conditions => 'memberships.manager = false'
+                      :conditions => 'memberships.manager = 0'
 
   has_many :managers, :through    => :memberships, 
                       :source     => :user, 
-                      :conditions => 'memberships.maneger = true'
+                      :conditions => 'memberships.manager = 1'
 
   validates :name, :presence => true
 end
