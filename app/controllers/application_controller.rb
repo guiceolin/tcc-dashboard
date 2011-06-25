cf3611233cf96d96af8203eabf897c853bbe0638
Guilherme Ceolin <guiceolin@gmail.com>
2011-06-25 01:04:48 -0300

    Define after sign_in path to dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,12 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
   end
+
   protect_from_forgery
+
+  private
+
+  def after_sign_in_path_for(user)
+    desktop_url
+  end
 end
