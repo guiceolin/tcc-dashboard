44aa6ba268d1a29db45ea444edb266d07fcd9ab3
Guilherme Ceolin <guiceolin@gmail.com>
2011-07-23 13:42:55 -0300

    Tasks API first implementation

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,21 @@ class Task < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
 
-  def completed?
-    end_date.present? && end_date < Time.now
+  def start
+    !start_date.present? && !!(start_date = Time.now)
   end
+
+  def started?
+    start_date.present?
+  end
+
+  def complete
+    start_date.present? && !!(end_date = Time.now)
+  end
+
+  def completed?
+    end_date.present?
+  end
+
+
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -4,15 +4,35 @@ describe Task do
   it { should belong_to :project }
   it { should belong_to :user }
 
-  let(:start_project) { Fabricate :task, :start_date => 2.days.ago, :end_date => 1.day.ago }
-  let(:finish_project) { Fabricate :task, :start_date => 2.days.ago, :end_date => 1.day.from_now }
-  let(:finish_project_layout) { Fabricate :task, :start_date => 2.days.ago }
-
   context "#completed?" do
+    let(:start_project) { Fabricate :task, :start_date => 2.days.ago, :end_date => 1.day.ago }
+    let(:finish_project) { Fabricate :task, :start_date => 2.days.ago }
+
     it { start_project.completed?.should be true }
     it { finish_project.completed?.should be false }
-    it { finish_project_layout.completed?.should be false }
   end
 
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "#complete" do
+    let(:unstarted_task) { Fabricate :task }
+    let(:started_task)   { Fabricate :task, :start_date => 2.days.ago }
+
+    it { unstarted_task.complete.should be false }
+    it { started_task.complete.should be true }
+  end
+
+  context "started?" do
+    let(:unstarted_task) { Fabricate :task }
+    let(:started_task)   { Fabricate :task, :start_date => 2.days.ago }
+
+    it { started_task.started?.should be true }
+    it { unstarted_task.started?.should be false }
+  end
+
+  context "start" do
+     let(:unstarted_task) { Fabricate :task }
+    let(:started_task)   { Fabricate :task, :start_date => 2.days.ago }
+
+    it { started_task.start.should be false }
+    it { unstarted_task.start.should be true }
+  end
 end
