408ed458f282d5a404b71aa6c1f622af32dcc0ab
Guilherme Ceolin <guiceolin@gmail.com>
2011-09-09 02:05:45 -0300

    Change status from delivery

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,4 +1,18 @@
 class Delivery < ActiveRecord::Base
   belongs_to :message
   belongs_to :user
+
+  scope :unread, :conditions => { :read => false}
+
+  def unread?
+    !read
+  end
+
+  def unread!
+    self.read = false
+  end
+
+  def read!
+    self.read = true
+  end
 end
