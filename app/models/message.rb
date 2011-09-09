255f77cb93ea0d30095fb4afc562ff61f92a5ee9
Guilherme Ceolin <guiceolin@gmail.com>
2011-09-09 02:05:58 -0300

    After create message, sent it

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,14 @@ class Message < ActiveRecord::Base
   belongs_to :sender, :class_name => 'User'
   has_many :deliveries
   has_many :receivers, :through => :deliveries, :source => :user
+
+  after_save :send_message
+
+  private
+
+  def send_message
+    receivers.each do |r|
+      Delivery.new :user_id => r, :message => self, :read => false
+    end
+  end
 end
