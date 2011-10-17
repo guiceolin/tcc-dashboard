b8323addd0b44ff2e11cb90776411160604f8742
Guilherme Ceolin <guiceolin@gmail.com>
2011-10-17 18:25:50 -0200

    test for user#to_s

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,8 +9,18 @@ describe User do
   it { should have_many(:tasks) }
 
   context '#full_name' do
+
+
     let(:user) { Fabricate :member , :first_name => 'John', :last_name => 'Locke' }
     subject { user.full_name }
     it { should == 'John Locke' }
+
+
+  end
+
+  context '#to_s' do
+    let(:user) { Fabricate :member , :email => 'a@b.com', :first_name => 'John', :last_name => 'Locke' }
+    subject { user.to_s }
+    it { should == 'John[a@b.com]'}
   end
 end
