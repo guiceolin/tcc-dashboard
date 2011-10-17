210515abdffb09e2d7e2c8f541223060055feae5
Guilherme Ceolin <guiceolin@gmail.com>
2011-10-17 18:21:36 -0200

    Jquery-ui

--- config/application.rb
+++ config/application.rb
@@ -46,7 +46,7 @@ module Dashboard
     config.i18n.default_locale = :'pt-BR'
 
     # JavaScript files you want as :defaults (application.js is always included).
-    config.action_view.javascript_expansions[:defaults] = %w(jquery.min rails)
+    config.action_view.javascript_expansions[:defaults] = %w(jquery.min jquery-ui.min rails)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
