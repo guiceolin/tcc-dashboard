29066af9bf143ff8d4ac89e1d28fe37e8d9992ee
Guilherme Ceolin <guiceolin@gmail.com>
2011-10-31 20:22:32 -0200

    Manager desktop now shows master project

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,22 @@
 class HomeController < ApplicationController
 
   def desktop
-    @project = Project.find(current_user.current_project)
-    render :layout => 'application'
+    if current_user.is_a? Manager
+      manager_desktop
+    else
+      @project = current_user.current_project
+      render 'desktop', :layout => 'application'
+    end
   end
 
   def home
 
   end
 
+  private
+
+  def manager_desktop
+    @project = current_user.current_master_project
+    render 'manager_desktop', :layout => 'application'
+  end
 end
