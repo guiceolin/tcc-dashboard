54d3abdac4202b2ba8542fe07474d7b013a2106a
Guilherme Ceolin <guiceolin@gmail.com>
2011-11-08 20:01:35 -0200

    update repo when update memberships

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,5 +2,11 @@ class Membership < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
 
+  after_save :update_repo
+
   validates :user_id, :uniqueness => { :scope => :project_id }
+
+  def update_repo
+    project.save
+  end
 end
