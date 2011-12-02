eb659152c3c52e83ceeee6a00e188f5872651da4
Guilherme Ceolin <guiceolin@gmail.com>
2011-12-01 22:17:27 -0200

    helper for versions

--- app/helpers/versions_helper.rb
+++ app/helpers/versions_helper.rb
@@ -1,11 +1,15 @@
 module VersionsHelper
   def formated_version version
-    return '&nbsp'.html_safe unless object_hash = yaml_load(version.object)
+    return '&nbsp'.html_safe unless object_hash = yaml_load(version.try(:object))
     object_hash.map do |k,v|
       "#{I18n.t(k.to_sym, :scope => [:activerecord,:attributes, version.item_type.downcase])}:#{v}"
     end.join('<br />').html_safe
+  end
 
-
+  def formated_object object
+    object.attributes.map do |k,v|
+      "#{I18n.t(k.to_sym, :scope => [:activerecord,:attributes, object.class.name.downcase])}:#{v}"
+    end.join('<br />').html_safe
   end
 
   private
