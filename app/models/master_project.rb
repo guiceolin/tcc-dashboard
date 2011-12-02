cb8094b56b6e00150d0ec735c9514ec57c837b2a
Guilherme Ceolin <guiceolin@gmail.com>
2011-12-01 22:18:09 -0200

    fixing master project model

--- app/models/master_project.rb
+++ app/models/master_project.rb
@@ -1,12 +1,22 @@
 class MasterProject < ActiveRecord::Base
+  has_paper_trail
+
+  validates :description, :presence => true
+  validates :name, :presence => true, :uniqueness => true
+
   belongs_to :owner, :class_name => 'User'
   has_many :projects, :autosave => true
   has_many :messages, :autosave => true
+
+  def master_project
+    self
+  end
 
   def finish
     projects.each(&:finish)
     messages.each(&:archive)
   end
+
   def calendar_string
     CGI.escape("#{name} #{end_date.to_s}")
   end
