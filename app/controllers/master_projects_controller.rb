b325732f3019aaae47892f860e640ffd41b6ea07
Guilherme Ceolin <guiceolin@gmail.com>
2011-12-01 22:17:58 -0200

    master project controller fixing

--- app/controllers/master_projects_controller.rb
+++ app/controllers/master_projects_controller.rb
@@ -1,4 +1,7 @@
 class MasterProjectsController < ApplicationController
+
+  before_filter :find_current
+
   def index
     @masters = current_user.master_projects
   end
@@ -8,29 +11,47 @@ class MasterProjectsController < ApplicationController
   end
 
   def edit
-    @master = MasterProject.find(params[:id])
   end
 
   def create
     @master = MasterProject.new(params[:master_project])
     @master.owner = current_user
-    @master.save
+    respond_to do |format|
+      if @master.save
+        format.html { redirect_to(@master, :notice => 'Projeto criado com sucesso!') }
+        format.xml  { render :xml => @master, :status => :created, :location => @master }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @master.errors, :status => :unprocessable_entity }
+      end
+    end
   end
 
   def update
-    @master = MasterProject.find(params[:id])
     @master.update_attributes params[:master_project]
-    @master.save
+    respond_to do |format|
+      if @master.save
+        format.html { redirect_to(@master, :notice => 'Projeto criado com sucesso!') }
+        format.xml  { render :xml => @master, :status => :created, :location => @master }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @master.errors, :status => :unprocessable_entity }
+      end
+    end
   end
 
   def show
-    @master = MasterProject.find(params[:id])
+    @master = MasterProject.find_by_id(params[:id])
   end
 
   def finish
-    @master = current_user.current_master_project
     @master.finish
     @master.save
     redirect_to @master
+  end
+  private
+
+  def find_current
+    @master = current_user.current_master_project
   end
 end
