a842a4de2a984928634ce2185433774239a35297
Guilherme Ceolin <guiceolin@gmail.com>
2011-12-01 22:18:43 -0200

    refactoring projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,6 @@
 class ProjectsController < ActionController::Base
   load_and_authorize_resource :project
+  before_filter :find_current
 
   layout 'application'
 
@@ -8,23 +9,16 @@ class ProjectsController < ActionController::Base
   end
 
   def edit
-    @project = Project.find(params[:id])
   end
 
   def show
-    @project = Project.find(params[:id])
-    render 'home/desktop'
   end
 
   def index
     @projects = current_user.projects
-    @managed_projects = current_user.managed_projects if current_user.is_a? Manager
-    #flash[:alert] = 'Mensagem de Teste do Flash'
-    render :layout => 'application'
   end
 
   def update
-    @project = Project.find(params[:id])
 
     respond_to do |format|
       if @project.update_attributes(params[:project])
@@ -53,22 +47,7 @@ class ProjectsController < ActionController::Base
 
   end
 
-  def update
-    @project = Project.find(params[:id])
-
-    respond_to do |format|
-      if @project.update_attributes(params[:project])
-        format.html { redirect_to(@project, :notice => 'Project was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @project.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
   def destroy
-    @project = Project.find(params[:id])
     @project.destroy
 
     respond_to do |format|
@@ -78,7 +57,6 @@ class ProjectsController < ActionController::Base
   end
 
   def users
-    @project  = Project.find(params[:id])
     @members  = @project.members
     @managers = @project.managers
   end
@@ -90,18 +68,16 @@ class ProjectsController < ActionController::Base
       current_user.current_project_id = params[:id]
     end
     current_user.save
-    redirect_to desktop_path
+    redirect_to current_user.current_project
   end
 
   def finish
-    @project = current_user.current_project
     @project.finish
     @project.save
     redirect_to(@project)
   end
 
   def tree
-    @project = Project.find_by_id(params[:id])
     @repo = @project.repo
 
     if params[:commit_id].present?
@@ -120,6 +96,16 @@ class ProjectsController < ActionController::Base
       render 'tree'
     else
       render 'blob'
+    end
+  end
+
+  private
+
+  def find_current
+    if Manager === current_user
+      @project = Project.find_by_id(params[:id])
+    else
+      @project = current_user.current_project
     end
   end
 end
